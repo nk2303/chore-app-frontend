bd65686b71008565b1233003663861a6d7c722fd
Jonny <jonathanriecke@gmail.com>
2020-04-13 11:21:03 -0700

    merge changes made

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <React.StrictMode>
     <App />,
+  </React.StrictMode>
   document.getElementById('root')
 );
 
