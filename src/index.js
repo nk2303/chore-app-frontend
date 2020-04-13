5390ef062875007e971e1a15601c72e8f2b34029
Jonny <jonathanriecke@gmail.com>
2020-04-13 11:21:55 -0700

    fixed comma

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />,
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
