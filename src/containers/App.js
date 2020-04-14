0927f085b67d5745428857366015e33ebf1f01fe
dianakw8591 <dianakw@gmail.com>
2020-04-13 20:37:30 -0700

    current user checking in progress

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import NavBar from './NavBar'
-import Landing from './Landing'
-import Account from './Account'
-import HouseContainer from './HouseContainer'
+import NavBar from './NavBar';
+import Landing from './Landing';
+import Account from './Account';
+import HouseContainer from './HouseContainer';
+import { api } from '../services/api';
 
 // Routing between login (new user), account(user without a house) and house pages(user already belonging to a house)
 
@@ -16,25 +17,27 @@ class App extends React.Component {
     };
   }
 
-  // componentDidMount() {
-  //   const token = localStorage.getItem("token");
-  //   console.log(token)
-  //   if (token) {
-  //     // console.log('there is a token');
-  //     // make a request to the backend and find our user
-  //     api.auth.getCurrentUser().then(user => {
-  //       // console.log(user)
-  //       const updatedState = { ...this.state.auth, user: user };
-  //       this.setState({ auth: updatedState });
-  //     });
-  //   }
-  // }
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+    console.log(token)
+    if (token) {
+      api.auth.getCurrentUser().then(user => {
+        this.setState({ authUser: user.user });
+      });
+    }
+  }
 
   login = data => {
-    // const updatedState = { ...this.state.auth, user: {data.user} };
     localStorage.setItem("token", data.jwt);
     this.setState({ authUser: data.user });
   };
+
+  logout = () => {
+    localStorage.removeItem("token");
+    this.setState({ authUser: {}, });
+  };
+
+  
 
 
   render() {
