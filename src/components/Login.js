9c7c5c1e06509cd01cdb2fb29b618910a76a6205
dianakw8591 <dianakw@gmail.com>
2020-04-16 23:13:07 -0700

    login validation complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,80 +11,103 @@ export default class Login extends Component {
     super();
     this.state = {
       error: false,
+      validated: false,
       username: '',
       password: ''
     };
   }
 
 
-handleUsernameChange = event => {
-  this.setState({
-    username: event.target.value
-  })
-}
+  handleUsernameChange = event => {
+    this.setState({
+      username: event.target.value
+    })
+  }
 
-handlePasswordChange = event => {
-  this.setState({
-    password: event.target.value
-  })
-}
+  handlePasswordChange = event => {
+    this.setState({
+      password: event.target.value
+    })
+  }
 
 
-handleSubmit = event => {
-  event.preventDefault()
-  const user = {user: 
-    {username: this.state.username,
-     password: this.state.password,
-  }}
-  api.auth.login(user).then(resp => {
-    if (!resp.error) {
-      this.props.onLogin(resp);
-      if (resp.user.location_id) {
-        // make fetch to location show and route to /house
-        // set state to include all returned info
-        this.props.history.push('/house');
-      } else {
-        this.props.history.push('/account');
-      }
+  handleSubmit = event => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      this.setState({validated: true});
     } else {
-      this.setState({ error: true });
-      console.log(resp.error)
-      //do something useful if there's an error
+      event.preventDefault()
+      const user = {
+        user:
+        {
+          username: this.state.username,
+          password: this.state.password,
+        }
+      }
+      api.auth.login(user).then(resp => {
+        if (!resp.error) {
+          this.props.onLogin(resp);
+          if (resp.user.location_id) {
+            // make fetch to location show and route to /house
+            // set state to include all returned info
+            this.props.history.push('/house');
+          } else {
+            this.props.history.push('/account');
+          }
+        } else {
+          this.setState({ 
+            error: resp.error,
+            validated: false,
+            username: '',
+            password: '' 
+          });
+        }
+      });
     }
-  });
-};
+  };
 
-render() {
-  return (
-    <div>
-    <Container>
-        <Form.Label>Have an account?</Form.Label>
-        <br/>
-        <br/>
-        <Form onSubmit={event => this.handleSubmit(event)}>
-          <Form.Group>
-            <Form.Control
-              type="text"
-              label="username"
-              placeholder="Username"
-              onChange={event => this.handleUsernameChange(event)}
-              value={this.state.username}
-            />
-          </Form.Group>
-          <Form.Group>
-          <Form.Control 
-            type="password"
-            placeholder="Password"
-            onChange={event => this.handlePasswordChange(event)}
-            value={this.state.password}
-          />
-          </Form.Group>
-          <Button variant="secondary" type="submit" block>
-            Log in 
+  render() {
+    return (
+      <div>
+        <Container>
+          <Form.Label>Have an account?</Form.Label>
+          <br />
+          <br />
+          <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Control
+                required
+                type="text"
+                label="username"
+                placeholder="Username"
+                onChange={event => this.handleUsernameChange(event)}
+                value={this.state.username}
+              />
+              <Form.Control.Feedback type="invalid">
+                You must enter a username.
+            </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                onChange={event => this.handlePasswordChange(event)}
+                value={this.state.password}
+              />
+              <Form.Control.Feedback type="invalid">
+                  Enter your password.
+            </Form.Control.Feedback>
+            {this.state.error ? <Form.Text> {this.state.error} </Form.Text> : null}
+            </Form.Group>
+            <Button variant="secondary" type="submit" block>
+              Log in
           </Button>
-      </Form>
-    </Container>
-    </div>
-  )
+          </Form>
+        </Container>
+      </div>
+    )
   }
 }
