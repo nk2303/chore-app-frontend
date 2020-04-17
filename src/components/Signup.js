a928f9ae9d6d29daf14ce8da25d65c87451c2c61
dianakw8591 <dianakw@gmail.com>
2020-04-17 07:20:27 -0700

    error handling for signup, minor changes to login form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,13 +4,12 @@ import Form from 'react-bootstrap/Form'
 import { api } from '../services/api';
 import '../App.css';
 
-// Signup page, styling copied from Login page
-
 export default class Signup extends Component {
   constructor() {
     super();
     this.state = {
       error: false,
+      validated: false,
       fields: {
         username: '',
         first_name: '',
@@ -18,7 +17,7 @@ export default class Signup extends Component {
         email: '',
         password: '',
         password_confirmation: '',
-      }  
+      }
     };
   }
 
@@ -29,117 +28,143 @@ export default class Signup extends Component {
   };
 
   handleSubmit = event => {
-    event.preventDefault();
-    if (this.state.fields.password !== this.state.fields.password_confirmation) {
-      console.log('password and password confirmation do not match')
-      this.setState(prev => {
-        return({
-          fields: {
-            ...prev.fields, password: '', password_confirmation: '',
-            }
-        })
-      })
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      this.setState({ validated: true });
     } else {
-      const user = {user: this.state.fields};
-      api.user.newUser(user).then(resp => {
-        if (!resp.error) {
-          this.props.onSignin(resp);
-          this.props.history.push('/account');
-        } else {
-          this.setState({ error: true });
-          console.log(resp.error)
-          //do something useful if there's an error
-          this.setState({
+      event.preventDefault();
+      if (this.state.fields.password !== this.state.fields.password_confirmation) {
+        this.setState(prev => {
+          return ({
+            error: "Password and password confirmation do not match.",
+            validated: false,
             fields: {
-              username: '',
-              first_name: '',
-              last_name: '',
-              email: '',
-              password: '',
-              password_confirmation: '',
-            } 
+              ...prev.fields, password: '', password_confirmation: '',
+            }
           })
-        }
-      });
-    }  
+        })
+      } else {
+        const user = { user: this.state.fields };
+        api.user.newUser(user).then(resp => {
+          if (!resp.error) {
+            this.props.onSignin(resp);
+            this.props.history.push('/account');
+          } else {
+            this.setState({
+              error: resp.error,
+              validated: false,
+              fields: {
+                username: '',
+                first_name: '',
+                last_name: '',
+                email: '',
+                password: '',
+                password_confirmation: '',
+              }
+            })
+          }
+        });
+      }
+    }
   };
 
-render() {
+  render() {
     return (
-    <div>
-    <Container>
-      <Form.Label>Don't have an account?</Form.Label>
-      <br/>
-      <br/>
-      <Form onSubmit={event => this.handleSubmit(event)}>
-        <Form.Group >
-          <Form.Control
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={event => this.handleChange(event)}
-            value={this.state.fields.username}
-          />
-          </Form.Group>
-        <Form.Group>
-        {/* <Form.Label>First Name:</Form.Label> */}
-        <Form.Control
-          type="text"
-          name="first_name"
-          placeholder="First name"
-          onChange={event => this.handleChange(event)}
-          value={this.state.fields.first_name}
-        />
-        </Form.Group>
-        <Form.Group>
-        {/* <Form.Label>Last Name:</Form.Label> */}
-        <Form.Control
-          type="text"
-          name="last_name"
-          placeholder="Last name"
-          onChange={event => this.handleChange(event)}
-          value={this.state.fields.last_name}
-        />
-        </Form.Group>
-        <Form.Group>
-        {/* <Form.Label>Email address</Form.Label> */}
-        <Form.Control
-          type="text"
-          name="email"
-          placeholder="Enter email"
-          onChange={event => this.handleChange(event)}
-          value={this.state.fields.email}
-        />
-        </Form.Group>
-        <Form.Group>
-        {/* <Form.Label>Password</Form.Label> */}
-        <Form.Control 
-          type="password"
-          name="password"
-          placeholder="Password"
-          onChange={event => this.handleChange(event)}
-          value={this.state.fields.password}
-        />
-        </Form.Group>
-        <Form.Group>
-        {/* <Form.Label>Confirm Password:</Form.Label> */}
-        <Form.Control
-          type="password"
-          name="password_confirmation"
-          placeholder="Confirm password"
-          onChange={event => this.handleChange(event)}
-          value={this.state.fields.password_confirmation}
-        />
-        </Form.Group>
-        <Form.Group>
-        <Button variant="secondary" type="submit" block>
-            Create account
-        </Button>
-        </Form.Group>
-      </Form>
-        
-    </Container>
-    </div>
-  )
+      <div>
+        <Container>
+          <Form.Label>Don't have an account?</Form.Label>
+          <br />
+          <br />
+          <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+            <Form.Group >
+              <Form.Control
+                required
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={event => this.handleChange(event)}
+                value={this.state.fields.username}
+              />
+              <Form.Control.Feedback type="invalid">
+                You must enter a username.
+            </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                required
+                type="text"
+                name="first_name"
+                placeholder="First name"
+                onChange={event => this.handleChange(event)}
+                value={this.state.fields.first_name}
+              />
+              <Form.Control.Feedback type="invalid">
+                You must enter a first name.
+            </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                required
+                type="text"
+                name="last_name"
+                placeholder="Last name"
+                onChange={event => this.handleChange(event)}
+                value={this.state.fields.last_name}
+              />
+            </Form.Group>
+            <Form.Control.Feedback type="invalid">
+              You must enter a last name.
+            </Form.Control.Feedback>
+            <Form.Group>
+              <Form.Control
+                required
+                type="text"
+                name="email"
+                placeholder="Enter email"
+                onChange={event => this.handleChange(event)}
+                value={this.state.fields.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                You must enter an email.
+            </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                required
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={event => this.handleChange(event)}
+                value={this.state.fields.password}
+              />
+              <Form.Control.Feedback type="invalid">
+                You must enter a password.
+            </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                required
+                type="password"
+                name="password_confirmation"
+                placeholder="Confirm password"
+                onChange={event => this.handleChange(event)}
+                value={this.state.fields.password_confirmation}
+              />
+              <Form.Control.Feedback type="invalid">
+                You must confirm your password.
+            </Form.Control.Feedback>
+            {this.state.error ? <Form.Text> {this.state.error} </Form.Text> : null}
+            </Form.Group>
+            <Form.Group>
+              <Button variant="secondary" type="submit" block>
+                Create account
+              </Button>
+            </Form.Group>
+          </Form>
+        </Container>
+      </div>
+    )
   }
 }
