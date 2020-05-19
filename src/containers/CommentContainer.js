706de0e911138cf4e8961c86c03ca1328ba3f146
nk2303 <ngankhong03@gmail.com>
2020-05-19 16:00:57 -0700

    navabar ccss fixed

--- src/containers/CommentContainer.js
+++ src/containers/CommentContainer.js
@@ -16,7 +16,7 @@ export default class CommentContainer extends Component {
                 </div>
                 <main id="comment-bar" className="col-10 ">
                     
-                    <div className="white-trans-bg minHeight-300">Comments</div>
+                    <div className="white-trans-bg minHeight-300 pad-10">Comments</div>
                     
                 </main>
 
